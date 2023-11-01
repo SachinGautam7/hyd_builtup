f90e2a8a864c8ac6af0e3ea4d9bf88457393f22c
SachinGautam7 <127063640+SachinGautam7@users.noreply.github.com>
2023-11-02 00:16:21 +0530

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
 
     # Read the HTML file into a string
-    with open("hyd_builtup.html", "r") as f:
+    with open("hyd_builtup/hyd_builtup.html", "r") as f:
         html_string = f.read()
 
     # Display the HTML file in Streamlit
