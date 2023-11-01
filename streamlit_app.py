1dde981bca72d80a782814b5dc99c5349c92e0c5
SachinGautam7 <127063640+SachinGautam7@users.noreply.github.com>
2023-11-02 00:09:40 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def main():
+
+    # Read the HTML file into a string
+    with open("hyd_builtup.html", "r") as f:
+        html_string = f.read()
+
+    # Display the HTML file in Streamlit
+    st.components.html(html_string)
+
+if __name__ == "__main__":
+    main()
