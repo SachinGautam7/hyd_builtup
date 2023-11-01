748743e1999adc7f0a4aae7c84752b5bb8994fbc
SachinGautam7 <127063640+SachinGautam7@users.noreply.github.com>
2023-11-02 00:46:12 +0530

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,25 @@
 import streamlit as st
+import io
+import json
 
-path_to_html = "./hyd_builtup.html" 
+def read_html_file(filename):
+  with io.open(filename, "r", encoding="utf-8") as f:
+    html = f.read()
+  return html
 
-# Read file and keep in variable
-with open(path_to_html,'r') as f: 
-    html_data = f.read()
+html = read_html_file("./hyd_builtup.html")
 
-## Show in webpage
-st.components.v1.html(html_data)
+# Render the HTML file in a hidden iframe
+iframe_html = """
+<iframe id="fullscreen-iframe" style="display: none;"></iframe>
+"""
+
+st.components.html(iframe_html)
+
+# Expand the iframe to fullscreen using JavaScript
+js_code = """
+document.getElementById("fullscreen-iframe").style.display = "block";
+document.getElementById("fullscreen-iframe").requestFullscreen();
+"""
+
+st.components.v1.html(js_code, height=0, width=0)
