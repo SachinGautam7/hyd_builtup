61023afdef5d3364af9f970a1c88366c84dbe1d6
SachinGautam7 <127063640+SachinGautam7@users.noreply.github.com>
2023-11-03 07:21:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,11 @@
 import streamlit as st
 
-from streamlit_js_eval import streamlit_js_eval
+path_to_html = "./hyd_builtup.html" 
 
-st.write(f"Screen width is {streamlit_js_eval(js_expressions='screen.width', key = 'SCR')}")
-st.write(f"Screen height is {streamlit_js_eval(js_expressions='screen.height', key = 'SCR1')}")
+# Read file and keep in variable
+with open(path_to_html,'r') as f: 
+    html_data = f.read()
 
-# path_to_html = "./hyd_builtup.html" 
-
-# # Read file and keep in variable
-# with open(path_to_html,'r') as f: 
-#     html_data = f.read()
-
-# st.set_page_config(layout='wide')
-# ## Show in webpage
-# st.components.v1.html(html_data, height=)
+st.set_page_config(layout='wide')
+## Show in webpage
+st.components.v1.html(html_data, height=None)
